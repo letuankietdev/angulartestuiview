8e66b76d18860389f5cb81cdf9fc869a151393a3
Kiet Le <letuankietdhth@gmail.com>
2018-10-01 14:38:20 +0700

    update code igniter
    qweqwe

---
+++ demo-angular-codeigniter/application/controllers/Example_api.php
@@ -0,0 +1,14 @@
+<?php
+    $name = 'KIET';
+
+      echo "Tao la".$name."ne";
+// require(APPPATH .'libraries/REST_Controller.php');
+
+// class Example_api extends REST_Controller{
+//     public  function test_get(){
+//         $name = 'KIET';
+
+//         echo "Tao la".$name."ne";
+//     }
+// }
+?>
